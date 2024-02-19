6df44397088480ec6bbb48d3f70d40368d9997a2
QuocHung <hungthanh1412hz@gmail.com>
2024-02-19 10:55:18 +0700

    add transition for hover

--- feature/hover.js
+++ feature/hover.js
@@ -1,26 +1,33 @@
 const container_element = document.querySelectorAll('.container_element');
 const container_text = document.querySelectorAll('.container_text');
 
+const vx = 0.1 + '%';
+const vy = 0.1 + '%';
+
 
 function hover(){
     document.onmousemove = (e) => {
-        const x = e.clientX*10/window.innerWidth + '%';
-        const y = e.clientY*10/window.innerWidth + '%';
+        const x = e.pageX*20/window.innerWidth + '%';
+        const y = e.pageY*20/window.innerWidth + '%';
 
         container_element.forEach((element)=>{
             element.style.transform = `translate(${x}, ${y})`;
-            // element.style.transform = `translateY(${y})`;
+            element.style.transition = `transform 1s ease-out`;
         })
+
+        
 
         container_text.forEach((text)=>{
             text.style.transform = `translate(${x}, ${y})`;
-            // text.style.transform = `translateY(${y})`;
+            text.style.transition = `transform 1s ease-out`;
+
         })
 
-        console.log('hi!');
-
-
     }
+}
+
+function move(xAim, yAim){
+
 }
 
 
